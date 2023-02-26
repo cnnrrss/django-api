5cf1f571feb52d241d4a81eea87635e415fbd9a3
Connor Vanderhook <14183191+cnnrrss@users.noreply.github.com>
2023-02-26 20:51:31 +0800

    feat: enable user mgmt api for team members using DRF

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forum.apps.ForumConfig',  # This object was created in /forum/apps.py
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF config
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
