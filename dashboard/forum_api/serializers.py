f992d37a5dcdf2a9cfb9a0caae19b1c2a0cd870e
Connor Vanderhook <14183191+cnnrrss@users.noreply.github.com>
2023-02-28 21:03:00 +0800

    feat: custom user manager class to extend django

--- dashboard/forum_api/serializers.py
+++ dashboard/forum_api/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        # fields = []
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'date_joined']
 
 
 class PostSerializer(serializers.ModelSerializer):
